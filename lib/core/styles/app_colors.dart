8ec74d1d19cb0d8ea53bec5e5b0fd3bf69ec7e9c
AhmedHafez32 <mohahz38556@gmail.com>
2026-04-06 17:13:28 +0200

    add app colors

--- lib/core/styles/app_colors.dart
+++ lib/core/styles/app_colors.dart
@@ -1,3 +1,23 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
+  static const Color primaryColor = Color(0xff2e7d32);
+  static const Color secondaryColor = Color(0xff66bb6a);
+
+  static const Color blackColor = Color(0xff000000);
+
+  static const Color whiteColor = Color(0xffffffff);
+
+  static const Color greyColor = Color(0xff6b7280);
+  static const Color lightGreyColor = Color(0xffe5e7eb);
+  static const Color darkGreyColor = Color(0xff111827);
+
+
+
+  static const Color redColor = Color(0xffef4444);
+
+
+  static const Color blueColor = Color(0xff1d4ed8 );
+  static const Color lightBlueColor = Color(0xff60a5fa);
 
 }
